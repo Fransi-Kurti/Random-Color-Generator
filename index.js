f11e4d40794cd2848e641def6a829ff8a41b3c67
fransi-kurti <fransi.kurti@gmail.com>
2023-02-17 17:37:24 +0100

    Random Color Generator update(finished javascript)

--- index.js
+++ index.js
@@ -4,5 +4,28 @@ for (let i = 0; i < 50; i++) {
     const colorContainerEl = document.createElement("div");
     colorContainerEl.classList.add("colors");
     mainContainerEl.appendChild(colorContainerEl);
-    
+}
+
+const allColorContainers = document.querySelectorAll(".colors");
+
+colorGenerate();
+
+function colorGenerate(){
+    allColorContainers.forEach((colorContainerEl)=>{
+        const newColorCode = createRandomColor();
+        colorContainerEl.style.backgroundColor = "#" + newColorCode;
+        colorContainerEl.innerText = "#" + newColorCode;
+    });
+}
+
+function createRandomColor(){
+    const characters = "0123456789abcdef";
+    const colorCodeLengh = 6;
+    let colorCode = "";
+    for(let i = 0; i < colorCodeLengh; i++){
+        const randomNumber = Math.floor(Math.random() * characters.length);
+        colorCode += characters.substring(randomNumber, randomNumber + 1);
+        
+    }
+    return colorCode;
 }
